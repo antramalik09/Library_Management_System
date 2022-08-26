d62d67c16e0debe001b8a76483b79fda3b89db82
Antra Malik <amalik@axway.com>
2022-08-26 11:47:47 +0530

    Initial Commit

---
+++ Libarary-Management-System/src/main/java/com/sopra/LibararyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sopra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibararyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibararyManagementSystemApplication.class, args);
+	}
+
+}
